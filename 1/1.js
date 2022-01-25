e86f5585075fb4fb717de2cbe1d62e468fa50cd2
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-25 15:10:42 +0330

    modal created

---
+++ 1/1.js
@@ -0,0 +1,15 @@
+// const modal = document.getElementById("myModal");
+// const modalContent = document.getElementById("content");
+// const btn = document.getElementById("submit");
+// const span = document.getElementsByClassName("close")[0];
+ 
+// function openModal(){   
+//     modal.classList.toggle("modal");
+//     modalContent.classList.toggle("modal-comntent");
+
+//  }
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
