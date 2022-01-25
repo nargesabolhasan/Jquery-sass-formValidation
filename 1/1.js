6ed64b41d29b73f7462aeac09cd1668619610e3f
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-25 18:57:56 +0330

    function validation created

--- 1/1.js
+++ 1/1.js
@@ -1,16 +1,29 @@
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("submit");
 const closeicone = document.getElementsByClassName("close")[0];
+
+
 function openModal(e) {
   e.preventDefault()
   modal.style.display = "block";
   setTimeout(function() {location.reload();}, 8000);
 }
+
 closeicone.addEventListener("click",()=>{
     modal.style.display = "none";
-  })
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+})
+
+
+function validation(e){
+console.log(e.target)
+let string = document.getElementById("first").value.split("")
+ let text;
+   if(string.length<=3){
+      text = "Input not valid";
+   }else {
+       text = "Input OK";
+       }
+
+document.getElementById("demo").innerHTML = text;
+
+}
