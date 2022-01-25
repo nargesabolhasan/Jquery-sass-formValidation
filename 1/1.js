4bfdb490fdc272e14a5298074a1bedea199d8061
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-25 19:47:13 +0330

    validation finished

--- 1/1.js
+++ 1/1.js
@@ -4,40 +4,46 @@ const closeicone = document.getElementsByClassName("close")[0];
 
 
 function openModal(e) {
-  e.preventDefault()
-  modal.style.display = "block";
-  setTimeout(function() {location.reload();}, 8000);
+  const unvalid1 = document.getElementById("demo2").innerHTML
+  const unvalid2 = document.getElementById("demo").innerHTML
+  if (unvalid1 === "Input not valid!" || unvalid2 === "Input not valid!") {
+    e.preventDefault()
+  } else {
+    e.preventDefault()
+    modal.style.display = "block";
+    setTimeout(function () { location.reload(); }, 8000);
+  }
 }
 
-closeicone.addEventListener("click",()=>{
-    modal.style.display = "none";
+closeicone.addEventListener("click", () => {
+  modal.style.display = "none";
 })
 
 
-function validation(e){
-let string = document.getElementById("first").value.split("")
- let text;
-   if(string.length<=3){
-      text = "Input not valid!";
-      e.target.classList.add("br-color-danger")
-   }else {
-       text = "Input OK";
-       }
-
-document.getElementById("demo").innerHTML = text;
+function validation(e) {
+  let string = document.getElementById("first").value.split("")
+  let text;
+  if (string.length <= 3) {
+    text = "Input not valid!";
+    e.target.classList.add("br-color-danger")
+  } else {
+    text = "";
+    e.target.classList.remove("br-color-danger")
+  }
+  document.getElementById("demo").innerHTML = text;
 
 }
 
-function validation2(e){
+function validation2(e) {
   let string = document.getElementById("last").value.split("")
-   let text;
-     if(string.length<=3){
-        text = "Input not valid!";
-        e.target.classList.add("br-color-danger")
-     }else {
-         text = "Input OK";
-         }
-  
-  document.getElementById("demo2").innerHTML = text;
-  
+  let text;
+  if (string.length <= 3) {
+    text = "Input not valid!";
+    e.target.classList.add("br-color-danger")
+  } else {
+    text = "";
+    e.target.classList.remove("br-color-danger")
   }
+  document.getElementById("demo2").innerHTML = text;
+
+}
